a1169ea3ddc2237215c9b793ed15006fec48e048
Ankit <ankit.singh@leverageedu.com>
2024-12-08 12:00:49 +0530

    add some errors

--- routes/feed.js
+++ routes/feed.js
@@ -7,18 +7,19 @@ const router = express.Router();
 router.get('/:userId', authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req?.params?.userId);
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    // if (!user) return res.status(404).json({ message: 'User not found' });
 
     const friendPosts = await Post.find({ userId: { $in: user?.friends } });
     const commentedPosts = await Post.find({ "comments.userId": { $in: user?.friends } });
 
     const feed = [...friendPosts, ...commentedPosts].sort((a, b) => b.createdAt - a.createdAt);
 
-    console.log(feed, "All about feed details");
+    // console.log(feed, "All about feed details");
 
     res.json(feed);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    // res.status(500).json({ message: err.message });
+    console.log(err)
   }
 });
 
